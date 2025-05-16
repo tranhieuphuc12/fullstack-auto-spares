3fa2b4e1a5a5fbc81a554e51afb2b0c495f8d7c5
PhucTran <tranhieuphuc12@gmail.com>
2025-05-16 16:05:35 +0700

    [Fix] remove parent categories

--- frontend/src/app/components/HomeProductList.tsx
+++ frontend/src/app/components/HomeProductList.tsx
@@ -1,38 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import Category from '@/app/interfaces/ICategory'; 
+import Category from '@/app/interfaces/ICategory';
 import ProductList from '@/app/components/ProductsList';
 
 const HomeProductList: React.FC = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
-  const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [filteredCategories, setFilteredCategories] = useState<Category[]>([]);
+    const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 
-  useEffect(() => {
-	const fetchCategories = async () => {
-	  try {
-		// Replace the URL below with your actual categories API endpoint
-		const response = await fetch(`${API_BASE}/api/categories`);
-		if (!response.ok) {
-		  throw new Error('Failed to fetch categories');
-		}
-		const data = await response.json();
-		setCategories(data);
-	  } catch (error) {
-        console.error('Error fetching categories:', error);
-      }
-	};
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
 
-	fetchCategories();
-  }, []);
+                const response = await fetch(`${API_BASE}/api/categories`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch categories');
+                }
+                const data = await response.json();
+                setCategories(data);
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            }
+        };
 
-  return (
-	<div>
-        <ProductList API={`${API_BASE}/api/products?`}/>
-		{categories.map((category) => (            
-		  <ProductList key={category._id} API={`${API_BASE}/api/products/categoryId/${category._id}?`} title={category.name}/>
-		))}
-	 
-	</div>
-  );
+        fetchCategories();
+    }, []);
+    useEffect(() => {
+       // remove categoies with subcategories 
+        const filtered = categories.filter((category) => !category.subCategories || category.subCategories.length === 0);
+        setFilteredCategories(filtered);
+    }, [categories]);
+
+
+
+    return (
+        <div>
+            <ProductList API={`${API_BASE}/api/products?`} />
+            {filteredCategories.map((category) => (
+                <ProductList key={category._id} API={`${API_BASE}/api/products/categoryId/${category._id}?`} title={category.name} />
+
+            ))}
+
+        </div>
+    );
 };
 
 export default HomeProductList;
--- frontend/src/app/interfaces/ICategory.ts
+++ frontend/src/app/interfaces/ICategory.ts
@@ -3,5 +3,6 @@
 interface Category {
     _id: string;
     name: string;
+    subCategories: string[];
 }
 export default Category;
